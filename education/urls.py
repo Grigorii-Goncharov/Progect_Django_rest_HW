498aead34b876f2701edaa4f9acf24fd7a1a7894
grigorii goncharov <grigoriy85@gmail.com>
2025-09-14 20:27:06 +0300

    добавлены маршруты в приложения courses и lesson

--- education/urls.py
+++ education/urls.py
@@ -1,6 +1,20 @@
-from education.apps import EducationConfig
+from django.urls import path
+
+from .apps import EducationConfig
+from rest_framework.routers import DefaultRouter
+
+from .models import Lesson
+from .serializers import LessonSerializer
+from .views import CourseViewSet, LessonCreateList, LessonRetrieveUpdateDestroy
+
+router = DefaultRouter()
+router.register(r'courses', CourseViewSet, basename='course')
 
 app_name = EducationConfig.name
 
 urlpatterns = [
+    path('lesson/', LessonCreateList.as_view(queryset=Lesson.objects.all(), serializer_class=LessonSerializer), name='lesson-list'),
+    path('lesson/<int:pk>/', LessonRetrieveUpdateDestroy.as_view(queryset=Lesson.objects.all(), serializer_class=LessonSerializer), name='lesson-update'),
 ]
+
+urlpatterns += router.urls
