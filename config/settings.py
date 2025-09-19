be8453d3ca6fe4f3e157733c3e5bc5014aebc8f7
grigorii goncharov <grigoriy85@gmail.com>
2025-09-19 13:51:27 +0300

    установлент пакет  django-filter., внесены дополнения в setting.py в INSTALLED_APPS и добавлен REST_FRAMEWORK= {}

--- config/settings.py
+++ config/settings.py
@@ -30,11 +30,20 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'django_filters',
 
     'education',
     'users',
 
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': (
+        'django_filters.rest_framework.DjangoFilterBackend',
+        ...
+    ),
+}
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
