39e18308c81f248aad4b0dafddad9ba6b4542b35
grigorii goncharov <grigoriy85@gmail.com>
2025-09-19 13:45:17 +0300

    Для модели курса реализовано поле вывода уроков сериализатором CourseSerializerList для связанной модели.

--- education/serializers.py
+++ education/serializers.py
@@ -3,6 +3,11 @@ from rest_framework.serializers import ModelSerializer
 
 from education.models import Course, Lesson
 
+class LessonSerializer(ModelSerializer):
+    class Meta:
+        model = Lesson
+        fields = "__all__"
+
 
 class CourseSerializer(ModelSerializer):
 
@@ -13,16 +18,22 @@ class CourseSerializer(ModelSerializer):
 
 class CourseSerializerList(ModelSerializer):
     count_lessons = SerializerMethodField()
+    lessons= LessonSerializer(many= True, read_only=True)# many = уроков может быть несколько(берем сериализатор урока)
 
     def get_count_lessons(self, subj):
         return subj.lessons.count()
 
     class Meta:
         model = Course
-        fields = "__all__"
+        fields = [
+            'id',
+            'title',
+            'description',
+            'preview',
+            'count_lessons',
+            'lessons',
+        ]
 
 
-class LessonSerializer(ModelSerializer):
-    class Meta:
-        model = Lesson
-        fields = "__all__"
+
+
