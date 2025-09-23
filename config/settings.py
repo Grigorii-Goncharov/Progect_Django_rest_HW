407b327a1505f7771c8326d498eb50c3c371d8e3
grigorii goncharov <grigoriy85@gmail.com>
2025-09-23 11:19:10 +0300

    Настройка параметров settings.py DEFAULT_AUTHENTICATION_CLASSES и SIMPLE_JWT

--- config/settings.py
+++ config/settings.py
@@ -1,5 +1,4 @@
-
-
+from datetime import timedelta
 from pathlib import Path
 import os
 from dotenv import load_dotenv
@@ -41,6 +40,15 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': (
         'django_filters.rest_framework.DjangoFilterBackend',
     ),
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=15),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
 }
 
 
