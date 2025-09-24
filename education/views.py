2bee2429b75bd954c7a7b4f2db05b5e7604a3b21
grigorii goncharov <grigoriy85@gmail.com>
2025-09-24 12:16:26 +0300

    CourseViewSet через viewsets.ModelViewSet

--- education/views.py
+++ education/views.py
@@ -1,145 +1,63 @@
-from django.shortcuts import get_object_or_404
+from rest_framework.permissions import IsAuthenticated
+
 from education.models import Course, Lesson
-from education.serializers import (
-    CourseSerializer,
-    LessonSerializer,
-    CourseSerializerList,
-)
-from rest_framework import viewsets, status, generics
-from rest_framework.response import Response
+from education.serializers import CourseSerializer, CourseSerializerList, LessonSerializer
+
+from rest_framework import viewsets, generics
+
+from users.permissions import IsOwnerOrModerator
 
 
-class CourseViewSet(viewsets.ViewSet):
-    """
-    ViewSet для управления курсами (Course).
-    Реализует стандартные CRUD-операции через HTTP-методы:
-    - GET /courses/       — получение списка всех курсов
-    - POST /courses/      — создание нового курса
-    - GET /courses/{id}/  — получение одного курса по ID
-    - PUT /courses/{id}/  — полное обновление курса
-    - PATCH /courses/{id}/ — частичное обновление курса
-    - DELETE /courses/{id}/ — удаление курса
-    Использует CourseSerializer для сериализации/десериализации данных.
-    Все ответы возвращаются в формате JSON с корректными HTTP-статусами.
+class CourseViewSet(viewsets.ModelViewSet):
+    """ViewSet-класс для вывода списка курсов и информации по одному объекту
+    ModelViewSet - достаточен для передачи queryset и serializer и все работает из коробки
     """
 
-    def list(self, request):
-        """
-        Возвращает список всех курсов.
-        Метод: GET /courses/
-        """
-        queryset = Course.objects.all()
-        serializer = CourseSerializer(queryset, many=True)
-        return Response(serializer.data)
+    permission_classes = [IsAuthenticated, IsOwnerOrModerator]
 
-    def create(self, request):
-        """
-        Создаёт новый курс на основе переданных данных.
-        Метод: POST /courses/
-        Ожидаемые данные:
-            JSON-объект с полями, соответствующими модели Course (например, title, description, etc.).
-        """
-        serializer = CourseSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def get_serializer_class(self):
+        """Метод ловит действие 'retrieve', то перенаправляет на другой сериализатор"""
+        if self.action == "retrieve":
+            return CourseSerializerList
+        return CourseSerializer
 
-    def retrieve(self, request, pk=None):
-        """
-        Возвращает детальную информацию о курсе по его уникальному идентификатору.
-        Метод: GET /courses/{id}/
-        Аргументы:
-            pk (str): Уникальный идентификатор курса (первичный ключ).
-        Возвращает:
-            Response: Сериализованные данные курса с использованием CourseSerializerList.
-        """
-        course = get_object_or_404(Course.objects.all(), pk=pk)
-        serializer = CourseSerializerList(
-            course
-        )  # или CourseSerializer, если нужен детальный
-        return Response(serializer.data)
+    def get_queryset(self):
+        """Кто может смотреть все уроки или только свои"""
+        if self.request.user.groups.filter(name='Moderator').exists():
+            return Course.objects.all()
+        return Course.objects.filter(owner=self.request.user)
 
-    def update(self, request, pk=None):
-        """
-        Полностью обновляет существующий курс.
-        Метод: PUT /courses/{id}/
-        Заменяет все поля курса на переданные в запросе.
-        Если поле не передано — оно будет установлено в null/пустое значение.
-        Аргументы:
-            pk (str): Уникальный идентификатор курса.
-        """
-        course = get_object_or_404(Course.objects.all(), pk=pk)
-        serializer = CourseSerializer(course, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def partial_update(self, request, pk=None):
-        """
-        Частично обновляет существующий курс.
-        Метод: PATCH /courses/{id}/
-        Обновляет только те поля, которые переданы в теле запроса.
-        Остальные поля остаются без изменений.
-        Аргументы:
-            pk (str): Уникальный идентификатор курса.
-        """
-        course = get_object_or_404(Course.objects.all(), pk=pk)
-        serializer = CourseSerializer(course, data=request.data, partial=True)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def destroy(self, request, pk=None):
-        """
-        Удаляет курс по его ID.
-        Метод: DELETE /courses/{id}/
-        После удаления курс больше не доступен. Возвращает пустой ответ.
-        Аргументы:
-            pk (str): Уникальный идентификатор курса.
-        """
-        course = get_object_or_404(Course.objects.all(), pk=pk)
-        course.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    def perform_create(self, serializer):
+        """Автоприсваиание автора - владельца"""
+        serializer.save(owner=self.request.user)
 
 
 class LessonCreateList(generics.ListCreateAPIView):
-    """
-    API-эндпоинт для просмотра списка уроков и создания нового урока.
-    Поддерживает два HTTP-метода:
-        - GET: Получить список всех уроков.
-        - POST: Создать новый урок, отправив данные в теле запроса.
-    Использует LessonSerializer для преобразования данных между моделью и JSON.
-    Примеры:
-        GET     /lessons/      → Вернёт массив всех уроков.
-        POST    /lessons/      → Создаст урок из переданных данных.
-    """
-
+    """Viewset для создания и просмотра урока или списков урока"""
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
+    permission_classes = [IsOwnerOrModerator]# Распределение прав пользователя и модератора
 
     def perform_create(self, serializer):
-        lesson = serializer.save()
+        """Автоприсваиание автора - владельца"""
+        serializer.save(owner=self.request.user)
 
-
+    def get_queryset(self):
+        """Кто может смотреть все курсы или только свои"""
+        if self.request.user.groups.filter(name='Moderator').exists():
+            return Lesson.objects.all()
+        return Lesson.objects.filter(owner=self.request.user)
 
 
 class LessonRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
-    """
-    API-эндпоинт для просмотра, полного или частичного обновления и удаления одного урока.
-    Поддерживает три HTTP-метода:
-        - GET: Получить детальную информацию об уроке по его ID.
-        - PUT/PATCH: Обновить урок полностью (PUT) или частично (PATCH).
-        - DELETE: Удалить урок.
-    Использует LessonSerializer для сериализации данных.
-    Примеры:
-        GET     /lessons/{id}/       → Получить урок.
-        PUT     /lessons/{id}/       → Полностью заменить урок.
-        PATCH   /lessons/{id}/       → Обновить только указанные поля.
-        DELETE  /lessons/{id}/       → Удалить урок.
-    """
-
+    """Viewset для обновления и удаления урока"""
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
+    permission_classes = [IsOwnerOrModerator]
+
+    def get_queryset(self):
+        """Кто может смотреть все уроки или только свои"""
+        if self.request.user.groups.filter(name='Moderator').exists():
+            return Lesson.objects.all()
+        return Lesson.objects.filter(owner=self.request.user)
+
